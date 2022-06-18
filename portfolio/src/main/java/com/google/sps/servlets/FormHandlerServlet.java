d1ff2c265bd44512859424d2e4212f270ba1831c
Erica <ericachen70819@gmail.com>
2022-06-18 19:33:49 +0000

    Complete sentiment analysis

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -1,17 +1,3 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package com.google.sps.servlets;
 
 import java.io.IOException;
@@ -22,16 +8,17 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/form-handler")
 public class FormHandlerServlet extends HttpServlet {
-    @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-        // Get the value entered in the form.
-        String textValue = request.getParameter("text-input");
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-        // Print the value so you can see it in the server logs.
-        System.out.println("You submitted: " + textValue);
+    // Get the value entered in the form.
+    String textValue = request.getParameter("text-input");
 
-        // Write the value to the response so the user can see it.
-        response.getWriter().println("You submitted: " + textValue);
-    }
+    // Print the value so you can see it in the server logs.
+    System.out.println("You submitted: " + textValue);
+
+    // Write the value to the response so the user can see it.
+    response.getWriter().println("You submitted: " + textValue);
+  }
 }
